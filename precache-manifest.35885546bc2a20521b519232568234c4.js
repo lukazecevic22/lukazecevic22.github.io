8105a0e613814d1afc3b95504d720e65aea6aff2
Luka Zecevic <lukazecevic22@yahoo.com>
2020-04-03 11:45:31 +0200

    zavprojekat

---
+++ precache-manifest.35885546bc2a20521b519232568234c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d1ebc4626f2bfcb41452a523a0041f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d0f61abaee946805870d",
+    "url": "/my-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d7a9d1b00a966b60f5b7",
+    "url": "/my-app/static/css/main.844b3512.chunk.css"
+  },
+  {
+    "revision": "d0f61abaee946805870d",
+    "url": "/my-app/static/js/2.0a79264e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.0a79264e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a9d1b00a966b60f5b7",
+    "url": "/my-app/static/js/main.f58b41e9.chunk.js"
+  },
+  {
+    "revision": "48240a55f6c128dda452",
+    "url": "/my-app/static/js/runtime-main.4a944f77.js"
+  },
+  {
+    "revision": "39cd9d1214fc69906e686647e3b3964d",
+    "url": "/my-app/static/media/1.39cd9d12.jpg"
+  }
+]);
